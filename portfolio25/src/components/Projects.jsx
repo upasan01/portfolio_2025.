340e53099dec94b916f429dda01ac639e5196351
SANJOY <paulsanjoy2923@gmail.com>
2025-08-24 18:11:11 +0530

    new project added

--- portfolio25/src/components/Projects.jsx
+++ portfolio25/src/components/Projects.jsx
@@ -12,10 +12,33 @@ export const Projects = () => {
                     textColor='white'
                     borderColor='white'
                     shadowColor='black'>
-                    <p className='text-2xl'>Cross Platform Student Management App</p>
-                    <p className='text-dotgothic text-sm text-red-500'>This is a project which we made for our college, This app can run in both android and ios. The sole purpose of this app is save, manipulate and store the data of students in a structured manner. I basically did the backend part of this project.</p>
+                    <p className='text-2xl'>OuraCode</p>
+                    <p className='text-dotgothic text-sm text-red-500'>OuraCode is a collaborative code editor made by me and one of my friend, here you can code with your friends at realtime, here you can run the code in our terminal, you also can download code with its extension. And we also have out AuraFarmer AI which will help you to code more efficiently.</p>
                     <div className='flex flex-wrap lg:flex-nowrap gap-2'>
-                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>Flutter</Card>
+                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>ReactJS</Card>
+                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>ExpressJS</Card>
+                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>Docker</Card>
+                    </div>
+                    <div className='flex justify-end'>
+                        <Card borderColor='white' textColor='white' shadowColor='white'>
+                            <a
+                                className='flex items-center gap-2 font-xs'
+                                href='https://github.com/SANJOY-PAUL-0981/OuraCode'
+                                target='blank_'>Learn More <FaArrowRight /></a>
+                        </Card>
+                    </div>
+                </Card>
+
+                <Card
+                    className='p-4 lg:w-[25vw] w-[85vw] flex flex-col gap-5 justify-between'
+                    bg='transparent'
+                    textColor='white'
+                    borderColor='white'
+                    shadowColor='black'>
+                    <p className='text-2xl'>LearnAI</p>
+                    <p className='text-dotgothic text-sm text-red-500'>LearnAI is a smart chatbot platform that lets users have AI-powered conversations based on YouTube videos. Simply paste a video link, and LearnAI generates interactive, insightful chats to help you understand content better, faster, and in a more engaging way.</p>
+                    <div className='flex flex-wrap lg:flex-nowrap gap-2'>
+                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>MongoDB</Card>
                         <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>ExpressJS</Card>
                         <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>ReactJS</Card>
                     </div>
@@ -23,28 +46,7 @@ export const Projects = () => {
                         <Card borderColor='white' textColor='white' shadowColor='white'>
                             <a
                                 className='flex items-center gap-2 font-xs'
-                                href='https://github.com/SANJOY-PAUL-0981/Cross-Platform-Student-Management-App'
-                                target='blank_'>Learn More <FaArrowRight /></a>
-                        </Card>
-                    </div>
-                </Card>
-
-                <Card
-                    className='p-4 lg:w-[25vw] w-[85vw] flex flex-col gap-6 justify-between'
-                    bg='transparent'
-                    textColor='white'
-                    borderColor='white'
-                    shadowColor='black'>
-                    <p className='text-2xl'>Tsukifetch</p>
-                    <p className='text-dotgothic text-sm text-red-500'>Tsukifetch is a CLI based System Information tool like neofetch. From tsukifetch We can fetch system Informations like ram, gpu, processor, memory, used memory etc. Unfortunately it only works in Linux devices. It is build in C language. If you faced any kind of error feel free to contact me.</p>
-                    <div className='flex'>
-                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>C Language</Card>
-                    </div>
-                    <div className='flex justify-end'>
-                        <Card borderColor='white' textColor='white' shadowColor='white'>
-                            <a
-                                className='flex items-center gap-2 font-xs'
-                                href='https://github.com/SANJOY-PAUL-0981/Tsukifetch'
+                                href='https://github.com/SANJOY-PAUL-0981/LearnAI'
                                 target='blank_'>Learn More <FaArrowRight /></a>
                         </Card>
                     </div>
@@ -81,10 +83,10 @@ export const Projects = () => {
                     textColor='white'
                     borderColor='white'
                     shadowColor='black'>
-                    <p className='text-2xl'>LearnAI</p>
-                    <p className='text-dotgothic text-sm text-red-500'>LearnAI is a smart chatbot platform that lets users have AI-powered conversations based on YouTube videos. Simply paste a video link, and LearnAI generates interactive, insightful chats to help you understand content better, faster, and in a more engaging way.</p>
+                    <p className='text-2xl'>Cross Platform Student Management App</p>
+                    <p className='text-dotgothic text-sm text-red-500'>This is a project which we made for our college, This app can run in both android and ios. The sole purpose of this app is save, manipulate and store the data of students in a structured manner. I basically did the backend part of this project.</p>
                     <div className='flex flex-wrap lg:flex-nowrap gap-2'>
-                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>MongoDB</Card>
+                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>Flutter</Card>
                         <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>ExpressJS</Card>
                         <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>ReactJS</Card>
                     </div>
@@ -92,7 +94,28 @@ export const Projects = () => {
                         <Card borderColor='white' textColor='white' shadowColor='white'>
                             <a
                                 className='flex items-center gap-2 font-xs'
-                                href='https://github.com/SANJOY-PAUL-0981/LearnAI'
+                                href='https://github.com/SANJOY-PAUL-0981/Cross-Platform-Student-Management-App'
+                                target='blank_'>Learn More <FaArrowRight /></a>
+                        </Card>
+                    </div>
+                </Card>
+
+                <Card
+                    className='p-4 lg:w-[25vw] w-[85vw] flex flex-col gap-6 justify-between'
+                    bg='transparent'
+                    textColor='white'
+                    borderColor='white'
+                    shadowColor='black'>
+                    <p className='text-2xl'>Tsukifetch</p>
+                    <p className='text-dotgothic text-sm text-red-500'>Tsukifetch is a CLI based System Information tool like neofetch. From tsukifetch We can fetch system Informations like ram, gpu, processor, memory, used memory etc. Unfortunately it only works in Linux devices. It is build in C language. If you faced any kind of error feel free to contact me.</p>
+                    <div className='flex'>
+                        <Card borderColor='white' textColor='black' bg='white' className='font-semibold text-sm'>C Language</Card>
+                    </div>
+                    <div className='flex justify-end'>
+                        <Card borderColor='white' textColor='white' shadowColor='white'>
+                            <a
+                                className='flex items-center gap-2 font-xs'
+                                href='https://github.com/SANJOY-PAUL-0981/Tsukifetch'
                                 target='blank_'>Learn More <FaArrowRight /></a>
                         </Card>
                     </div>
